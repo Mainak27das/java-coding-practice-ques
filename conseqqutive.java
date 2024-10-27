2d5e3fdfcf6e884a91bd9a31e630e7b8c0886f4c
Mainak Das <dasmainak2018@gmail.ccom>
2024-10-27 11:02:49 +0530

    fixed some minor bugs

--- conseqqutive.java
+++ conseqqutive.java
@@ -15,17 +15,17 @@ public class conseqqutive {
     public static int find_largest_consequtive(int arr[]){
         int count=0;
         int maxcount=0;
-        for(int i=0;i<arr.length-1;i++){
+        for(int i=0;i<arr.length;i++){
             if(arr[i]==1){
                 
-                if(arr[i]==arr[i+1]){
+                
                     count++;
-                }
+                
             }
             
             else{
                 maxcount=Math.max(maxcount,count);
-                count=1;
+                count=0;
             }
 
         }
